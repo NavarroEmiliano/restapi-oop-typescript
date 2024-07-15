ec4b0f35da360512cc867e44c3d18fcb2260da1b
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-07-15 16:52:10 -0300

    hello world

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,21 @@ import cors from 'cors';
 
 class ServerBootstrap {
   public app: express.Application = express();
-  private port: number = 3000;
+  private readonly port: number = 3000;
 
   constructor() {
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(morgan('dev'));
     this.app.use(cors());
+
+    this.app.get('/api', (req, res) => {
+      res.status(200).json({ message: 'Hola mundo' });
+    });
     this.listen();
   }
 
-  public listen() {
+  private listen() {
     this.app.listen(this.port, () => {
       console.log(`🚀Server running on port ${this.port}`);
     });
