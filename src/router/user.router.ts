4773bbe77b7dfdf7e043b4b490a83bf2409725e4
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-07-18 20:25:54 -0300

    router

---
+++ src/router/user.router.ts
@@ -0,0 +1,12 @@
+import { BaseRouter } from '.';
+import { UserController } from '../controllers/user.controller';
+
+export class UserRouter extends BaseRouter<UserController> {
+  constructor() {
+    super(UserController);
+  }
+
+  routes(): void {
+    this.router.get('/user', (req, res) => this.controller.getUser(req, res));
+  }
+}
