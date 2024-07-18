1325fda1c72fa574d3f7becc89b7b1de37dc6cc3
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-07-18 20:51:19 -0300

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 export class BaseRouter<T> {
   public router: Router;
   public controller: T;
-  constructor(TController: { new (): T }) {
+  constructor(TController: new ()=> T) {
     this.router = Router();
     this.controller = new TController();
     this.routes();
