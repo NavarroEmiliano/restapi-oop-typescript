709995e97fbbd320be12c2a17dd8a54f1f679e53
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-07-21 02:54:58 -0300

    purchase entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,20 +1,30 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToOne } from 'typeorm';
 import { BaseEntity } from '../../config/base.entity';
+import { CustomerEntity } from '../../customer/entities/customer.entity';
 
 @Entity({ name: 'user' })
 export class UserEntity extends BaseEntity {
-  @Column()
-  username!: string;
-
   @Column()
   name!: string;
 
   @Column()
   lastname!: string;
 
-  @Column({ nullable: true })
-  jobPosition?: string;
+  @Column()
+  username!: string;
 
   @Column()
-  phoneNumber!: number;
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Column()
+  city?: string;
+
+  @Column({ nullable: true })
+  province!: number;
+
+  @OneToOne(() => CustomerEntity, (customer) => customer.user)
+  customer!: CustomerEntity;
 }
